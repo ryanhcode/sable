77e3ab5c0139bb83c05125b23fd26f026efbd0f8
sery <sery@sappho.company>
2026-05-14 04:05:21 +0200

    mixin compatibility improvements

--- common/src/main/java/dev/ryanhcode/sable/mixin/camera/camera_rotation/EntityMixin.java
+++ common/src/main/java/dev/ryanhcode/sable/mixin/camera/camera_rotation/EntityMixin.java
@@ -1,5 +1,6 @@
 package dev.ryanhcode.sable.mixin.camera.camera_rotation;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import dev.ryanhcode.sable.companion.math.JOMLConversion;
 import dev.ryanhcode.sable.companion.math.Pose3dc;
 import dev.ryanhcode.sable.mixinhelpers.camera.camera_rotation.EntitySubLevelRotationHelper;
@@ -22,8 +23,8 @@ public abstract class EntityMixin  {
 
     @Shadow private Level level;
 
-    @Inject(method = "calculateViewVector", at = @At("RETURN"), cancellable = true)
-    public void sable$calculateViewVector(final float f, final float g, final CallbackInfoReturnable<Vec3> cir) {
+    @ModifyReturnValue(method = "calculateViewVector", at = @At("RETURN"))
+    public Vec3 sable$calculateViewVector(final Vec3 viewVector, final float f, final float g) {
         final Function<SubLevel, Pose3dc> provider;
 
         if (this.level instanceof final LevelPoseProviderExtension levelPoseProvider) {
@@ -35,9 +36,10 @@ public abstract class EntityMixin  {
         final Quaterniond orientation = EntitySubLevelRotationHelper.getEntityOrientation((Entity) (Object) this, provider, 0.0f, EntitySubLevelRotationHelper.Type.CAMERA);
 
         if (orientation != null) {
-            final Vec3 viewVector = cir.getReturnValue();
-            cir.setReturnValue(JOMLConversion.toMojang(orientation.transform(JOMLConversion.toJOML(viewVector))));
+            return JOMLConversion.toMojang(orientation.transform(JOMLConversion.toJOML(viewVector)));
         }
+
+        return viewVector;
     }
 
 }
--- common/src/main/java/dev/ryanhcode/sable/mixin/clip_overwrite/BlockGetterMixin.java
+++ common/src/main/java/dev/ryanhcode/sable/mixin/clip_overwrite/BlockGetterMixin.java
@@ -1,8 +1,9 @@
 package dev.ryanhcode.sable.mixin.clip_overwrite;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import dev.ryanhcode.sable.ActiveSableCompanion;
 import dev.ryanhcode.sable.Sable;
-import dev.ryanhcode.sable.api.SubLevelHelper;
 import dev.ryanhcode.sable.companion.math.BoundingBox3d;
 import dev.ryanhcode.sable.companion.math.JOMLConversion;
 import dev.ryanhcode.sable.companion.math.Pose3dc;
@@ -15,42 +16,29 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.phys.shapes.VoxelShape;
-import org.jetbrains.annotations.NotNull;
 import org.joml.Vector3dc;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 
 import java.util.function.Predicate;
 
 /**
  * Overwrites raycasts to take sublevels into account
- *
- * TODO: The priority is currently higher simply to take priority over lithium, but usage of Lithium's raycast replacement alongside our sub-level stuff would be far nicer.
  */
-@Mixin(value = BlockGetter.class, priority = 1100)
+@Mixin(BlockGetter.class)
 public interface BlockGetterMixin {
 
     @Shadow
     BlockState getBlockState(BlockPos blockPos);
 
-    /**
-     * @author RyanH
-     * @reason Overwrites raycasts to take sublevels into account
-     */
-    @Overwrite
-    default BlockHitResult clip(ClipContext clipContext) {
-        final BlockGetter self = (BlockGetter) this;
-
+    @WrapMethod(method = "clip", order = Integer.MIN_VALUE)
+    default BlockHitResult clip(ClipContext clipContext, final Operation<BlockHitResult> original) {
         if (!(this instanceof final Level level) || (clipContext instanceof final ClipContextExtension extension && extension.sable$doNotProject())) {
             // If the level cannot have sublevels, use the original method
-            return originalClip(self, clipContext);
+            return original.call(clipContext);
         }
 
         final SubLevel ignoredSubLevel = clipContext instanceof final ClipContextExtension extension ?
@@ -93,7 +81,7 @@ public interface BlockGetterMixin {
             final Vec3 diff = clipContext.getFrom().subtract(clipContext.getTo());
             minResult = BlockHitResult.miss(clipContext.getTo(), Direction.getNearest(diff.x, diff.y, diff.z), BlockPos.containing(clipContext.getTo()));
         } else {
-            minResult = originalClip(self, clipContext);
+            minResult = original.call(clipContext);
             minDistance = minResult.getLocation().distanceTo(clipContext.getFrom());
         }
 
@@ -121,7 +109,8 @@ public interface BlockGetterMixin {
 
 
             final ClipContext subClipContext = new ClipContext(JOMLConversion.toMojang(from), JOMLConversion.toMojang(to), clipContext.block, clipContext.fluid, clipContext.collisionContext);
-            final BlockHitResult subResult = originalClip(subLevel.getLevel(), subClipContext);
+            ((ClipContextExtension) subClipContext).sable$setDoNotProject(true);
+            final BlockHitResult subResult = subLevel.getLevel().clip(subClipContext);
             final double distance = subResult.getLocation().distanceTo(subClipContext.getFrom());
 
             if ((distance < minDistance || minResult.getType() == HitResult.Type.MISS) && subResult.getType() != HitResult.Type.MISS) {
@@ -132,26 +121,4 @@ public interface BlockGetterMixin {
 
         return minResult;
     }
-
-    @Unique
-    private static @NotNull BlockHitResult originalClip(final BlockGetter level, final ClipContext clipContext) {
-        return BlockGetter.traverseBlocks(clipContext.getFrom(), clipContext.getTo(), clipContext, (clipContextx, blockPos) -> {
-            final BlockState blockState = level.getBlockState(blockPos);
-            final FluidState fluidState = level.getFluidState(blockPos);
-            final Vec3 vec3 = clipContextx.getFrom();
-            final Vec3 vec32 = clipContextx.getTo();
-            final VoxelShape voxelShape = clipContextx.getBlockShape(blockState, level, blockPos);
-            final BlockHitResult blockHitResult = level.clipWithInteractionOverride(vec3, vec32, blockPos, voxelShape, blockState);
-            final VoxelShape voxelShape2 = clipContextx.getFluidShape(fluidState, level, blockPos);
-            final BlockHitResult blockHitResult2 = voxelShape2.clip(vec3, vec32, blockPos);
-            final double d = blockHitResult == null ? Double.MAX_VALUE : clipContextx.getFrom().distanceToSqr(blockHitResult.getLocation());
-            final double e = blockHitResult2 == null ? Double.MAX_VALUE : clipContextx.getFrom().distanceToSqr(blockHitResult2.getLocation());
-            return d <= e ? blockHitResult : blockHitResult2;
-        }, clipContextx -> {
-            final Vec3 vec3 = clipContextx.getFrom().subtract(clipContextx.getTo());
-            return BlockHitResult.miss(clipContextx.getTo(), Direction.getNearest(vec3.x, vec3.y, vec3.z), BlockPos.containing(clipContextx.getTo()));
-        });
-    }
-
-
 }
